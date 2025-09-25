21323623156b24063267e571efc388abb76b0d31
ssedgwick <ssedgwick@cochlear.com>
2025-09-25 11:13:04 +1000

    Addressed some display issues on smaller formats

--- app/View/Calculator.hs
+++ app/View/Calculator.hs
@@ -19,38 +19,33 @@ viewCalculator x =
   [ H.h2_ [ P.className "s12" ] [ text "Calculator" ]
   , H.article_ [ P.className "s12 fill" ]
     [ H.article_ [] [ raceSelector x ]
-    , H.article_ []
-      [ H.table_ [ P.className "stripes" ]
-        [ H.thead_ []
-          [ H.tr_ []
-            [ H.th_ [] [ text "Attribute" ]
-            , H.th_ [ P.className "centertext" ] [ text "Base Score" ]
-            , H.th_ [ P.className "centertext" ] [ text "" ]
-            , H.th_ [ P.className "centertext" ] [ text "Racial Bonus" ]
-            , H.th_ [ P.className "centertext" ] [ text "" ]
-            , H.th_ [ P.className "centertext" ] [ text "Total Score" ]
-            , H.th_ [ P.className "centertext" ] [ text "Ability Modifier" ]
-            , H.th_ [ P.className "centertext" ] [ text "Point Cost" ]
-            ]
-          ]
-        , H.tbody_ [] ((map (mkAttributeRow x) allStats) ++ [ totalRow x ])
-        ]
-      ]
-      , H.article_ [ P.className "s12" ] ( H.h4_ [] [ text "Racial Traits"] : racialTraits (x ^. race) )
+    , H.article_ [ P.className "grid" ]
+      ( [ H.div_ [ P.className "s2" ] [ H.p_ [] [ H.b_ [] [ text "Attribute" ] ] ]
+        , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Base Score" ] ] ]
+        , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "" ] ] ]
+        , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Racial Bonus" ] ] ]
+        , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "" ] ] ]
+        , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Total Score" ] ] ]
+        , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Ability Modifier" ] ] ]
+        , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Point Cost" ] ] ]
+        ] 
+        <> (concatMap (mkAttributeRow x) allStats) 
+        <> totalRow x
+      )
+    , H.article_ [ P.className "s12" ] ( H.h4_ [] [ text "Racial Traits"] : racialTraits (x ^. race) )
     ]
   ]
 
-mkAttributeRow :: Model -> Stat -> View Model Action
+mkAttributeRow :: Model -> Stat -> [View Model Action]
 mkAttributeRow x s =
-    H.tr_ []
-    [ H.td_ [] [ text (ms $ show s) ]
-    , H.td_ [ P.className "centertext" ] [ numberField x sl (min, max) (ChangeInt sl) ]
-    , H.td_ [ P.className "centertext" ] [ text "+" ]
-    , H.td_ [ P.className "centertext" ] [ text (msshow (x ^. rbl)) ] -- in special cases, this can change (for selected races)
-    , H.td_ [ P.className "centertext" ] [ text "=" ]
-    , H.td_ [ P.className "centertext" ] [ H.b_ [] [ text (msshow total) ] ]
-    , H.td_ [ P.className "centertext" ] [ text (msshow mod) ]
-    , H.td_ [ P.className "centertext" ] [ text (msshow cost) ]
+    [ H.div_ [ P.className "s2" ] [ H.p_ [] [ text (ms $ show s) ] ]
+    , H.div_ [ P.className "s2" ] [ numberField x sl (min, max) (ChangeInt sl) ]
+    , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ text "+" ] ]
+    , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ text (msshow (x ^. rbl)) ]]  -- in special cases, this can change (for selected races)
+    , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ text "=" ] ]
+    , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text (msshow total) ] ] ]
+    , H.div_ [ P.className "s1" ] [ H.p_ [ P.className "center-align" ] [ text (msshow mod) ] ]
+    , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ text (msshow cost) ] ]
     ]
   where
     total = (x ^. sl) + (x ^. rbl)
@@ -61,17 +56,12 @@ mkAttributeRow x s =
     rbl = compose (statLens s) racialBonuses
     sl = compose (statLens s) stat
 
-totalRow :: Model -> View Model Action
+totalRow :: Model -> [View Model Action]
 totalRow x =
-  H.tr_ []
-  [ H.td_ [] []
-  , H.td_ [ P.className "centertext" ] [ H.button_ [ E.onClick ResetCustomValues ] [ text "Reset" ] ]
-  , H.td_ [] []
-  , H.td_ [] []
-  , H.td_ [] []
-  , H.td_ [] []
-  , H.td_ [] [ H.b_ [] [ text "Total Points" ] ]
-  , H.td_ [ P.className "centertext" ] [ text $ ms (show totalCost <> " / " <> show (x ^. availablePoints)) ]
+  [ H.div_ [ P.className "s2" ] [ H.button_ [ E.onClick ResetCustomValues ] [ text "Reset" ] ]
+  , H.div_ [ P.className "s6" ] []
+  , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ H.b_ [] [ text "Total Points" ] ] ]
+  , H.div_ [ P.className "s2" ] [ H.p_ [ P.className "center-align" ] [ text $ ms (show totalCost <> " / " <> show (x ^. availablePoints)) ] ]
   ]
   where
     totalCost = sum (map (\s -> x ^. (pointBuyCostValue (x ^. (compose (statLens s) stat)))) allStats)
